d98a68fd598758140c1d58e573e0fbaeb8f6708d
Sebastian Schäfer <bschr9uwb@googlemail.com>
2018-10-18 12:38:23 +0200

    Fixing Model Error -> Word Schema

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var WordSchema = require('../models/word');
+// var WordSchema = require('../models/word');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   // WordSchema.find({count: {$gt: 5}}, (err, words) => {
-  res.render('configurator/index', {words: words});
+  // res.render('configurator/index', {words: words});
   // });
 });
 
