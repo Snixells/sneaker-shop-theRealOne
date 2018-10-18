88e0a1481ecbb6f2e802ce5406bc6fed26793336
Sebastian Schäfer <bschr9uwb@googlemail.com>
2018-10-18 15:03:04 +0200

    Adding routes for specific pages

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // WordSchema.find({count: {$gt: 5}}, (err, words) => {
-   res.render('configurator/index');
-  // });
-  // 
-
+  res.render('configurator/index');
 });
+
+router.get('/configurator', function (req, res, next) {
+  res.render('configurator/configurator', { title: "Konfigurator" });
+});
+
+router.get('/registrieren', function (req, res, next) {
+  res.render('pages/register', { title: "Registrierung" });
+});
+
+router.get('/contact', function (req, res, next) {
+  res.render('pages/contact', { title: "Kontakt" });
+});
+
+router.get('/disclaimer', function (req, res, next) {
+  res.render('pages/disclaimer', { title: "Datenschutz"});
+});
+
+router.get('/imprint', function (req, res, next) {
+  res.render('pages/imprint', { title: "Impressum" });
+});
+
 
 module.exports = router;
