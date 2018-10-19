21d20266970d18708147affa3672732b42ef0b34
Sebastian Schaefer <Handyfreak011@gmail.com>
2018-10-19 15:50:20 +0200

    changed name fileds in register.hbs

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,7 @@ var User = require('../models/user');
 var LocalStrategy = require('passport-local').Strategy;
 
 passport.serializeUser(function(user,done){
-    done(null,user.id);
+    done(null,user.username);
 });
 
 passport.deserializeUser(function(id,done){
