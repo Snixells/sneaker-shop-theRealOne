8eba31f2fd3878a4924ce9ded045b6bff9c59b91
Sebastian Schäfer <bschr9uwb@googlemail.com>
2018-10-19 12:53:53 +0200

    Sign up with password

--- config/passport.js
+++ config/passport.js
@@ -39,7 +39,7 @@ passport.use('local.signup', new LocalStrategy({
 
 }));
 
-passport.user('local.signin',new LocalStrategy({
+passport.use('local.signin',new LocalStrategy({
     usernameField: 'username',
     passwordField: 'password',
     passReqToCallback: true},
